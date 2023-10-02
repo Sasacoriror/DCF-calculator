c930ed213c38a631ecd8b99245d0e2e6a1cee9cb
Sasacoriror <43811709+Sasacoriror@users.noreply.github.com>
2023-10-02 10:14:07 +0200

    Lagt til html med bruk av bootstrap

---
+++ src/main/java/com/example/kalkulatur_aksje/KalkulaturAksjeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kalkulatur_aksje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalkulaturAksjeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KalkulaturAksjeApplication.class, args);
+    }
+
+}
