d38cf616d567f8a4b1a62d6f0b7a168d4936f0d9
Sasacoriror <43811709+Sasacoriror@users.noreply.github.com>
2024-05-11 22:52:36 +0200

    Update Kalkulator.js

--- Kalkulator.js
+++ Kalkulator.js
@@ -30,7 +30,7 @@ function calculate(FCF, years, discount, growth, price){
         ut+="Overvalued";
     }
 
-    const sum = (FCF_Sum - price) / price;
+    const sum = (FCF_Sum - price) / ((FCF_Sum + price)/2);
     const percentage = sum * 100;
     let skrivUt = "Intrinsic value: $"+FCF_Sum.toFixed(2)+"\n"+ut+": "+percentage.toFixed(0)+"%";
 
